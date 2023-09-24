9a827b7b68ef066db30bea5cc17197c6fde796a8
Шикалов_Никита <muchik.33@yandex.ru>
2023-09-24 11:13:34 +0300

    start second task

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+// больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// System.Console.WriteLine("Введите количество чисел");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// int num2 = 0;
+// int num3 = 0;
+// for (int i = 0; i < num1; i++)
+// {
+//     System.Console.WriteLine("Введите число");
+//     num3 = Convert.ToInt32(Console.ReadLine());
+//     if (num3 > 0)
+//     {
+//         num2++;
+//     }
+// }
+// System.Console.WriteLine("количество чисел больше нуля " + num2);
+
+
+// string? num1 = "";
+// int num2 = 0;
+// int num3 = 0;
+// while ( num1 != "да")
+// {
+//     System.Console.WriteLine("Все числа введены?");
+//     num1 = Console.ReadLine();
+//     if (num1 == "нет")
+//     {
+//     System.Console.WriteLine("введите число");
+//     num3 = Convert.ToInt32(Console.ReadLine());
+//     }
+//     else if (num1== "да")
+//     {
+//         num3 = 0;
+//     }
+//     else
+//     {
+//         System.Console.WriteLine("Введите да или нет");
+//     }
+//     if (num3 > 0)
+//     {
+//         num2++;
+//     }
+// }
+// System.Console.WriteLine("количество чисел больше нуля " + num2);
+
+
+
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения 
+// двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
